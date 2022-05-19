570a49347a02625699fa09f3d24846ad8908fa53
kate patch <kate.epatch@gmail.com>
2022-05-19 15:32:42 -0600

    added var for getting the current hour to use in the function through moment

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ var today = moment();
 $("#currentDay").text(today.format("dddd, MMM DD, YY, h:mm a"));
 
 var time = $().parent().attr("id");
+var timeNow = moment().hours();
 
 function trackTime () {
-    
+    $(".time-block").each(function(){
+        
+    })
 }
